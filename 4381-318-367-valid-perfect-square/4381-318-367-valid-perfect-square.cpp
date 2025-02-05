2bd156c48a348526aab59b7639b3d2844ee33be6
mitansio <damitansio@gmail.com>
2025-02-05 11:32:30 +0300

    Time: 0 ms, Memory: 7.8 MB - LeetHub

---
+++ 4381-318-367-valid-perfect-square/4381-318-367-valid-perfect-square.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if(sqrt(num)-int (sqrt(num))==0.00){
+            return true;
+        }
+        else
+            return false;
+            
+    }
+};
